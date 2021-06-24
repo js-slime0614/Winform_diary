e8422c6cb4ff5b646b9a99527065550c1cc1cd1e
단단한슬라임 <66393239+js-slime0614@users.noreply.github.com>
2021-06-24 15:29:08 +0900

    Oracle Db Connect

--- winform_dotnet_diary/Form1.cs
+++ winform_dotnet_diary/Form1.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Oracle.ManagedDataAccess.Client;
+using System.Configuration;
 namespace winform_dotnet_diary
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OracleConnection con = new OracleConnection(ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+            try
+            {
+                con.Open();
+                MessageBox.Show("Connect");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex));
+            }
         }
     }
 }
